d7ce42c57054bd9e404b0fccb482fc64a9e8a904
Kevin <kklika@pursuantsolutions.com>
2016-05-12 01:53:36 -0500

    Finish Asset Management Updates
    Moved all functions to portal and updated how assets are called.

--- OpenGarage/www_assets/publish_assets.sh
+++ OpenGarage/www_assets/publish_assets.sh
@@ -1,7 +1,3 @@
 rm ../www_assets.h
-./publish_assets views/ap_home.html html_ap_home
-./publish_assets views/sta_home.html html_sta_home
-./publish_assets views/sta_options.html html_sta_options
-./publish_assets views/sta_update.html html_sta_update
-./publish_assets views/sta_logs.html html_sta_logs
-./publish_assets views/portal.html html_portal
+./publish_assets ap_home.html html_ap_home
+./publish_assets portal.html html_portal
